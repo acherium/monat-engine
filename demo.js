e260ef880cd53633f38c844588ce223390805502
Acherium <shinsu02@naver.com>
2024-08-16 01:20:46 +0900

    CSS 추가, 버튼 클래스 추가, 개체 생성 함수 개선

--- demo.js
+++ demo.js
@@ -1,4 +1,14 @@
-import { create, append, revoke } from "./lyra/lyra-script.js";
+import { append, create, LyraButton } from "./lyra/lyra-script.js";
 
 (() => {
+  const btn1 = new LyraButton({
+    id: "test-btn",
+    classes: [ "a", "b" ],
+    events: {
+      click: () => alert(true)
+    },
+    text: "123",
+    icon: "star5"
+  });
+  append(btn1);
 })();
--- lyra/lyra-script.js
+++ lyra/lyra-script.js
@@ -48,24 +48,30 @@ export const $a = (query, target = document) => target.querySelectorAll(query);
 
 /**
  * create 함수 매개변수 구조체
- * @typedef {object} lyraCreateParameters
- * @property {object} [attributes] HTML 요소에 적용할 속성값 객체 형식의 모음집.
- * @property {object} [events] HTML 요소에 적용할 이벤트 객체 형식의 모음집.
+ * @typedef {object} LyraCreateParameters
+ * @property {id} [id] 요소에 적용할 ID의 이름.
+ * @property {array} [classes] 요소에 적용할 클래스의 목록.
+ * @property {object} [attributes] 요소에 적용할 속성값(Attributes)의 객체 형식의 모음집.
+ * @property {object} [properties] 요소에 적용할 속성값(Properties)의 객체 형식의 모음집.
+ * @property {object} [events] 요소에 적용할 이벤트의 객체 형식의 모음집.
  */
 /**
  * 지정한 태그명의 HTML 요소를 만들어 반환합니다. 매개변수가 지정된 경우 지정된 값에 따라 속성이나 이벤트를 설정하고 반환합니다.
  * @param {string} tag HTML 요소명.
- * @param {lyraCreateParameters} [params] 
+ * @param {LyraCreateParameters} [params] 매개변수.
  * @returns {HTMLElement} 지정한 태그에 따라 반환합니다.
  * @see {@link https://developer.mozilla.org/ko/docs/Web/API/Document/createElement | MDN 레퍼런스> Document.createElement()}
  * @see {@link https://developer.mozilla.org/ko/docs/Web/API/Element/setAttribute | MDN 레퍼런스> Document.setAttribute()}
  * @see {@link https://developer.mozilla.org/ko/docs/Web/API/EventTarget/addEventListener | MDN 레퍼런스> EventTarget.addEventListener()}
  */
-export const create = (tag = "div", params = { attributes: {}, events: {} }) => {
+export const create = (tag = "div", params = { id: null, classes: [], attributes: {}, properties: {}, events: {} }) => {
   const res = document.createElement(tag);
   if (params) {
+    if (params.id && params.id !== null) res.id = params.id;
+    if (params.classes?.length > 0) for (const value of params.classes) res.classList.add(value);
     if (params.attributes) for (const key in params.attributes) res.setAttribute(key, params.attributes[key]);
-    if (params.events) for (const key in params.attributes) res.addEventListener(key, params.attributes[key]);
+    if (params.properties) for (const key in params.properties) res[key] = params.properties[key];
+    if (params.events) for (const key in params.events) res.addEventListener(key, params.events[key]);
   };
   return res;
 };
@@ -89,3 +95,35 @@ export const revoke = (node) => node.parentNode.removeChild(node);
 
 
 // 각종 클래스들
+export class LyraElement {
+  /**
+   * 원래 쓰던건데 아마 필요 없어질 듯
+   * @param {string} tag HTML 요소명.
+   * @param {LyraCreateParameters} [params] 매개변수.
+   * @return {LyraElement}
+   */
+  constructor(tag, params = { attributes: {}, events: {} }) {
+    this.$ = create(tag, params);
+    return this;
+  };
+};
+
+export class LyraButton {
+  /**
+   * @typedef {object} LyraButtonParametersRaw
+   * @property {string} [text] 버튼 텍스트.
+   * 
+   * @typedef {LyraCreateParameters & LyraButtonParametersRaw} LyraButtonParameters
+   */
+  /**
+   * 내 스타일의 버튼 생성
+   * @param {LyraButtonParameters} [params]
+   * @returns 
+   */
+  constructor(params) {
+    const button = create("button", params);
+    if (params.icon && params.icon.length > 0) append(create("div", { classes: [ "i", `i-${params.icon}` ] }), button);
+    if (params.text && params.text.length > 0) append(create("p", { properties: { innerText: params.text} }), button);
+    return button;
+  };
+};
