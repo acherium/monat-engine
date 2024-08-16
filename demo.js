059939dd49c078bc0f7ff8fbf10a06287718a619
Acherium <shinsu02@naver.com>
2024-08-16 13:25:47 +0900

    CSS 세포단위로 모듈화

--- demo.js
+++ demo.js
@@ -1,14 +1,4 @@
-import { append, create, LyraButton } from "./lyra/lyra-script.js";
+import { } from "./lyra/lyra-script.js";
 
 (() => {
-  const btn1 = new LyraButton({
-    id: "test-btn",
-    classes: [ "a", "b" ],
-    events: {
-      click: () => alert(true)
-    },
-    text: "123",
-    icon: "star5"
-  });
-  append(btn1);
 })();
