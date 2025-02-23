56af14fdd0bb71188fa2843e139310dcbf5f638f
Acherium <shinsu02@naver.com>
2025-02-23 20:35:08 +0900

    탭 다중 타겟 지원

--- lyra/lyra-init.js
+++ lyra/lyra-init.js
@@ -11,27 +11,34 @@ const init = (target) => {
   // 탭 기능 초기화
   const $tabs = $a(`:is(.tabs, .tabs-row, .tabs-column) > label:has(input[type="radio"])`, target);
   for (const $tab of $tabs) {
-    const $target = $(get($tab, "target"));
+    const $targets = $a(get($tab, "target"));
     const $radio = $(`input[type="radio"]`, $tab);
-    if (!$target || !$radio) continue;
+    if (!$targets || !$radio) continue;
 
     const name = get($radio, "name");
     const value = get($radio, "value");
     if (!name || !value) continue;
 
-    $target.style["display"] = $radio.checked ? null : "none";
+    for (const $target of $targets) {
+      $target.style["display"] = $radio.checked ? null : "none";
+    };
+
     $radio.onchange = () => {
       if (!$radio.checked) return;
       
       const $alts = $a(`label:has(input[type="radio"][name="${name}"]:not([value="${value}"]))`);
       for (const $alt of $alts) {
-        const $altTarget = $(get($alt, "target"));
-        if (!$altTarget) continue;
+        const $altTargets = $a(get($alt, "target"));
+        if (!$altTargets) continue;
 
-        $altTarget.style["display"] = "none";
+        for (const $altTarget of $altTargets) {
+          $altTarget.style["display"] = "none";
+        };
       };
 
-      $target.style["display"] = null;
+      for (const $target of $targets) {
+        $target.style["display"] = null;
+      };
     };
   };
 };
