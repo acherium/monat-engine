cd0d513fca5fcd4178874e9f086d47fc5c897289
Acherium <shinsu02@naver.com>
2025-03-07 22:08:16 +0900

    이름 변경

--- index.js
+++
@@ -1,4 +0,0 @@
-import * as l from "./lyra/lyra-module.js";
-
-(() => {
-})();
---
+++ monat/module.js
@@ -0,0 +1,31 @@
+// 모듈 로드
+export * from "./scripts/lyra-objman.js";
+export * from "./scripts/lyra-timeman.js";
+export * from "./scripts/lyra-domman.js";
+
+// 모듈 정보 변수 초기화
+export const LYRA_NAME = "Project Lyra";
+export const LYRA_DISPLAY_NAME = "Monat Engine";
+export const LYRA_AUTHOR = "Acherium";
+export const LYRA_CONTACT = "acherium@pm.me";
+export const LYRA_VERSION = "2000";
+export const LYRA_DATE = "25-03-2";
+
+// 기본 변수 초기화
+export const COMMON_INTERVAL = 30;
+export const ANIMATION_INTERVAL = 30;
+export const TOOLTIP_DURATION = 10000;
+export const TOOLTIP_ANIMATION_DURATION = 150;
+export const WINDOW_ANIMATION_DURATION = 150;
+export const WINDOW_ANIMATION_DURATION_LONG = 500;
+export const DEFAULT_NOTIFICATION_DURATION = 5000;
+export const DEFAULT_IMAGE_SLIDER_INTERVAL = 5000;
+export const DEFAULT_IMAGE_SLIDER_DURATION = 500;
+export const MINIMUM_WINDOW_WIDTH = 100;
+export const MINIMUM_WINDOW_HEIGHT = 100;
+export const DEFAULT_WINDOW_WIDTH = 600;
+export const DEFAULT_WINDOW_HEIGHT = 400;
+export const DEFAULT_WINDOW_X = 10;
+export const DEFAULT_WINDOW_Y = 10;
+
+export const DRAG_SCROLLING_THRESHOLD = 50;
